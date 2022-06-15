e78dea7dc499d50ee8e559d560b0ffcb309281d1
Thomas Schreiner <iteano.thom@hotmail.com>
2022-06-15 14:52:45 -0300

    Criando arquivos JS

---
+++ Exercicio_01/script.js
@@ -0,0 +1,4 @@
+let valor1 = 10
+let valor2 = "10"
+let comparacaoDeValores = valor1 === valor2
+console.log(comparacaoDeValores)
---
+++ Exercicio_02/script.js
@@ -0,0 +1,4 @@
+let usuario1Altura = parseFloat(1.80)
+let usuario2Altura = parseFloat(1.75)
+let comparacaoDeAlturas = usuario1Altura > usuario2Altura
+console.log(comparacaoDeAlturas)
